2ac1c962ab2948a790f8233ef03149ebf74f2e3c
afer <koudaiwang@koudaiwangdeMac-mini.local>
2016-10-18 17:04:53 +0800

    xxx
    xxx

--- XPusher/ViewController.m
+++ XPusher/ViewController.m
@@ -8,6 +8,13 @@
 
 #import "ViewController.h"
 
+#import "NWHub.h"
+#import "NWNotification.h"
+#import "NWPushFeedback.h"
+#import "NWPusher.h"
+#import "NWSSLConnection.h"
+#import "NWSecTools.h"
+
 @implementation ViewController
 
 
@@ -15,8 +22,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     _devButton.state = 1;
-
+    [self loadCertificatesFromKeychain];
 }
+
+- (void)loadCertificatesFromKeychain {
+    NSError *error = nil;
+    NSArray *certs = [NWSecTools keychainCertificatesWithError:&error];
+    if (!certs) {
+        NWLogWarn(@"Unable to access keychain: %@", error.localizedDescription);
+    }
+    if (!certs.count) {
+        NWLogWarn(@"No push certificates in keychain.");
+    }
+    certs = [certs sortedArrayUsingComparator:^NSComparisonResult(NWCertificateRef a, NWCertificateRef b) {
+        NWEnvironmentOptions envOptionsA = [NWSecTools environmentOptionsForCertificate:a];
+        NWEnvironmentOptions envOptionsB = [NWSecTools environmentOptionsForCertificate:b];
+        if (envOptionsA != envOptionsB) {
+            return envOptionsA < envOptionsB;
+        }
+        NSString *aname = [NWSecTools summaryWithCertificate:a];
+        NSString *bname = [NWSecTools summaryWithCertificate:b];
+        return [aname compare:bname];
+    }];
+    NSMutableArray *pairs = @[].mutableCopy;
+    for (NWCertificateRef c in certs) {
+        [pairs addObject:@[c, NSNull.null]];
+    }
+//    _certificateIdentityPairs = [_certificateIdentityPairs arrayByAddingObjectsFromArray:pairs];
+}
+
 
 
 - (IBAction)buttonPressed:(NSPopUpButton *)sender {
